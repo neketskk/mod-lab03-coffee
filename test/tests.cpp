42bc12e5feadea3837c09648d2d7fc95e139db0e
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-04-26 02:06:04 +0300

    test1

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,42 @@
-// Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task1, test1) 
+{
+    Automata a = Automata();
+    a.on();
+    a.cancel();
+    int result = a.getState();
+    EXPECT_EQ(1, result);
+}
+
+TEST(task2, test2) 
+{
+    Automata a = Automata();
+    a.on();
+    a.coin(1);
+    a.choice(1);
+    int result = a.getState();
+    EXPECT_EQ(2, result); 
+}
+
+TEST(task3, test3) 
+{
+    Automata a = Automata();
+    a.on();
+    a.coin(200);
+    a.choice(1);
+    int result = a.getState();
+    EXPECT_EQ(4, result); 
+}
+
+TEST(task4, test4) {
+    Automata a = Automata();
+    a.on();
+    a.coin(100);
+    a.choice(2);
+    a.coin(100);
+    a.choice(2);
+    int result = a.getState();
+    EXPECT_EQ(4, result);
+}
